d063ffc32a36f6ed0a2550c727ddb693ef617695
yangdabei <89772815+yangdabei@users.noreply.github.com>
2026-04-01 11:45:03 +0000

    Deploying to gh-pages from @ yangdabei/yangdabei.github.io@6afc5c76c20eb46ab23ce485d8fffec3f9d06668 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/research/";
           },
-        },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "nav-fun",
           title: "fun",
           description: "",
